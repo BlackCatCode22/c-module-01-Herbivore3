20048efc4cb7186e0f7620aded14f6b1c9bdce34
Herbivore3 <0964178@my.scccd.edu>
2025-01-16 21:47:36 -0800

    Add files via upload

---
+++ simpleGreeting.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name = "Andy"; // Replace "YourName" with the desired name
+    std::cout << "Hi my name is " << name << "!" << std::endl;
+    return 0;
+}
